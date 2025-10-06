a9e12acb796686a8bc84867c3285b7baf793d768
Santiago <santolan31@gmail.com>
2025-10-06 12:22:33 -0500

    Fix background inicio

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -13,7 +13,7 @@ const Hero = () => {
   };
 
   return (
-    <section className="position-relative min-vh-100 d-flex align-items-center" style={{backgroundImage: 'url(./assets/images/background.png)', backgroundSize: 'cover', backgroundPosition: 'center'}}>
+    <section className="position-relative min-vh-100 d-flex align-items-center" style={{backgroundImage: 'url(/assets/images/background.png)', backgroundSize: 'cover', backgroundPosition: 'center'}}>
       <div className="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-75"></div>
       <div className="container position-relative z-1">
         <div className="row justify-content-center text-center">
